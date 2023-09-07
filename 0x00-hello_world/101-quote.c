18712d38ad296d882302808969f87af15b6b9093
abdelghafour99 <abdelghafour83@live.fr>
2023-09-07 15:38:56 -0700

    C - Hello, World commit 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
 * main - Print program
 *
-* Return: Always 0
+* Return: Always 1
 */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
