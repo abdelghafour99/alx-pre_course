89a36e10686d431549431ffa45e2fd2870f7b2ad
abdelghafour99 <abdelghafour83@live.fr>
2023-09-07 15:06:03 -0700

    C - Hello, World commit 4444

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - C PROGRAM 
- * prints Programming is like building a multilingual puzzle
- * Using puts
- * Return: Always 0
- */
+* main - Print program
+*
+* Return: Always 0
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
