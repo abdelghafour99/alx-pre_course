4003a010160d3be05a171b6f96d08a9c5634d633
abdelghafour99 <abdelghafour83@live.fr>
2023-09-08 16:38:58 -0700

    C - Variables, if, else, while commit 0000

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		puts("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		puts("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		puts("%d is zero\n", n);
 	}
 	return (0);
 }
