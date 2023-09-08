69d2117234993f2c45b96072dfc0b4a2e0951b87
abdelghafour99 <abdelghafour83@live.fr>
2023-09-08 16:58:35 -0700

    C - Variables, if, else, while commit 0000

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0
+*/
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if ((n % 10) > 0)
+	{
+		printf("Last digit of %d and is greater than5\n", n, n % 10);
+	}
+	else if (n < 0)
+	{
+		printf("Last digit of %d and is less than6 and not 0\n", n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
+
+	return (0);
+}
