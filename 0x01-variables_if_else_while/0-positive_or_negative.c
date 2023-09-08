baa98156544402f44c98ab010e68e7387a4ab9c0
abdelghafour99 <abdelghafour83@live.fr>
2023-09-08 16:42:23 -0700

    C - Variables, if, else, while commit 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,17 +10,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+
 	if (n > 0)
 	{
-		puts("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		puts("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		puts("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
+
 	return (0);
 }
