902f49206086ce0a808aefadd64e1763f968c205
abdelghafour99 <abdelghafour83@live.fr>
2023-09-08 18:02:38 -0700

    C - Variables, if, else, while commit 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints numbers
+ *        followed by a new line
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		putchar((a % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
