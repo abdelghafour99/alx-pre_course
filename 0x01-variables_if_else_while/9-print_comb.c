380380785429ea107d3432aac3384d878c3d9d88
abdelghafour99 <abdelghafour83@live.fr>
2023-09-08 18:21:04 -0700

    C - Variables, if, else, while commit 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single-digit numbers
+ *        followed by a new line
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar((a % 10) + '0');
+		if (a == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
