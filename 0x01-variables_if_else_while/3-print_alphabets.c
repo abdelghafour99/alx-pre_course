9aca0fab53e56ada3eb310bfd62850862dd6ac2a
abdelghafour99 <abdelghafour83@live.fr>
2023-09-08 17:30:16 -0700

    C - Variables, if, else, while commit 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ *        followed by a new line
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
