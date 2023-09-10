ff9ba8746252f95dc9ec18a9b8a1aa2438eff82e
abdelghafour99 <abdelghafour83@live.fr>
2023-09-10 14:43:45 -0700

    C - Variables, if, else, while commit 102102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,9 @@ int main(void)
 			c = a;
 			while (c <= 9)
 			{
-				d = b + 1;
+				if (a == 0 && b == 0 && c == 0)
+					d = b + 1;
+				d = 0;
 				while (d <= 9)
 				{
 					putchar((a % 10) + '0');
