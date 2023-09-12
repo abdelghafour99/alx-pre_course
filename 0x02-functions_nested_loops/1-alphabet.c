4659f4f063be24a15c3edfdce50e8c8fcf3c54f2
abdelghafour99 <abdelghafour83@live.fr>
2023-09-12 15:30:02 -0700

    0x02. C - Functions, nested loops commit 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,18 @@
 #include "_putchar.c"
 
 /**
- * main - print _alphabet
+ * print_alphabet - print alphabet
  *
- * Return: Always 0.
+ *
+ *
  */
 void print_alphabet(void)
 {
-	for (a = "a", a <= "z", a++)
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
 	{
-		putchar(a);
-		putchar('\n');
+		_putchar(a);
 	}
-	return (0);
+	_putchar('\n');
 }
