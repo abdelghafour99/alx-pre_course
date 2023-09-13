c7bd96334b32849c7c6990e7bfd1116d737887a9
abdelghafour99 <abdelghafour83@live.fr>
2023-09-12 18:07:50 -0700

    C - Hello, World commit 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print alphabet
+ *
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (b = 0; b < 24; b++)
+	{
+		for (a = 0; a < 59; a++)
+		{
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar(':');
+			_putchar((a / 10) + '0');
+			_putchar((a / 10) + '0');
+			_putchar(('\n');
+		}
+	}
+}
