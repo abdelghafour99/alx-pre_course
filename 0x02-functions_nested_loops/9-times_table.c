8d63b943d3e038a70334671f7f646481c336546d
abdelghafour99 <abdelghafour83@live.fr>
2023-09-12 19:16:30 -0700

    C - Hello, World commit 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,19 +14,21 @@ void times_table(void)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			if ((a * b) > 9)
+			if ((a * b) == 0)
+				_putchar((a * b) + '0');
+			else if ((a * b) > 9)
 			{
-				_putchar(((a * b) / 10) + '0');
-				_putchar(((a * b) % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar(((a * b) / 10) + '0');
+				_putchar(((a * b) % 10) + '0');
 			}
 			else
 			{
-				_putchar((a * b) + '0');
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar((a * b) + '0');
 			}
 
 		}
