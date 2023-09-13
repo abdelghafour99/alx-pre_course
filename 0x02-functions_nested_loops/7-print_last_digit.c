71852f21b1aaec6e42db57eac9d39665d861d2ae
abdelghafour99 <abdelghafour83@live.fr>
2023-09-12 17:57:01 -0700

    C - Hello, World commit 77

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,13 @@ int print_last_digit(int n)
 {
 	int b;
 
-	if (n > 0)
-	{
-		b = n % 10;
-	}
-	else
-	{
-		b = ((0 - 1) * n) % 10;
-	}
+	if (n < 0)
+		n = -n;
+	b = n % 10;
+
+	if (b < 0)
+		b = -b;
+
 	_putchar(b + '0');
 	return (b);
 
