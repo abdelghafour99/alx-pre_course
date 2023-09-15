cd8313cf95f79617f4084756d8b5ffff8129d02f
abdelghafour99 <abdelghafour83@live.fr>
2023-09-15 16:37:36 -0700

    0x04. C - More functions, more nested loops commit 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @a : the first number
+ * @b : the second number
+ * Return: 1
+ */
+int _isdigit(int a, int b)
+{
+	return (a * b);
+
+}
