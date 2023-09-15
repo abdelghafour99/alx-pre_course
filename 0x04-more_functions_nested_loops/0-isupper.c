3c823b5d8b7da0ab681973bc7b5bc188644b8d55
abdelghafour99 <abdelghafour83@live.fr>
2023-09-14 17:33:59 -0700

    0x04. C - More functions, more nested loops commit 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checksfor uppercase
+ *@c :the caracter checks
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if( c >= 41 && c <= 60)
+		return (1);
+	return (0)
+
+}
