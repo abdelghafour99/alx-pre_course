c70e41845783af79f97fa783b68292b9018ee7cd
abdelghafour99 <abdelghafour83@live.fr>
2023-09-16 16:22:55 -0700

    C - Hello, World commit 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - print n times table
+ *
+ * @n: the one be checked
+ *
+ */
+void print_times_table(int n)
+{
+	int a, b;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (a = 0 ; a <= n ; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				if (b == 0)
+					printf("0");
+				else
+				{
+					printf("%d", a);
+					printf(", ");
+				}
+			}
+		}
+	printf("\n");
+	}
+
+}
