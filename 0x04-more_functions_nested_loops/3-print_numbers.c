77468b2de13eb848a1baa5ae97adc1e622b1af41
abdelghafour99 <abdelghafour83@live.fr>
2023-09-16 16:05:09 -0700

    0x04. C - More functions, more nested loops commit 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('%d', a);
+	}
+	_putchar('\n');
+
+}
