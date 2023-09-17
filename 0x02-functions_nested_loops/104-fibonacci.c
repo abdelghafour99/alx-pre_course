df939ac861cc8580766f95cca4f05a9911aa138e
abdelghafour99 <abdelghafour83@live.fr>
2023-09-16 17:50:47 -0700

    C - Hello, World commit 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - 50-fibonacci
+ *
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	long int b1, b2, b3;
+
+	b1 = 1;
+	b2 = 1;
+
+	printf("%ld", b2);
+
+	for (a = 1; a < 98; a++)
+	{
+		b3 = b1;
+		b1 = b2;
+		b2 = b1 + b3;
+		printf(", ");
+		printf("%ld", b2);
+	}
+	printf("\n");
+	return (0);
+}
