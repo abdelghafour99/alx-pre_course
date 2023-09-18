4716e74445e5911151953e322901be36b8536868
abdelghafour99 <abdelghafour83@live.fr>
2023-09-18 14:53:27 -0700

    0x05. C - Pointers, arrays and strings commit 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - the length of a string
+ *
+ * @s: the number check
+ * Return: len(S)
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
