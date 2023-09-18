da878bde644dcffbe3341f28d27d10162b186420
abdelghafour99 <abdelghafour83@live.fr>
2023-09-18 16:54:50 -0700

    0x05. C - Pointers, arrays and strings commit 9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,14 @@ void print_array(int *a, int n)
 {
 	int b;
 
-
-	for (b = 0; b <= (n - 1); b++)
+	printf("%d", a[0]);
+	for (b = 1; b <= (n - 1); b++)
 	{
-		printf("%d, ", a[b]);
+		printf(", %d", a[b]);
 	}
 
 	if (b == (n - 1))
-		printf("%d, ", a[n - 1]);
+		printf(", %d", a[n - 1]);
 	printf("\n");
 
 }
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy- the length of a string
+ *
+ * @src: the first string
+ * @dest: the second string
+ * Return: String
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0, n;
+
+	while (*(src + 1) != '\0')
+	{
+		a++;
+	}
+	a--;
+	for (n = 0; n < a; n++)
+	{
+		dest[n] = src[n];
+	}
+	dest[a] = '\n';
+
+	return (dest);
+
+}
