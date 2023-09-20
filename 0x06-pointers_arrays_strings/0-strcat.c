10c27e586ebba4ab8ae908a6a9aa0e2604258b85
abdelghafour99 <abdelghafour83@live.fr>
2023-09-20 16:52:08 -0700

    0x06. C - More pointers, arrays and strings commit 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates two strings
+ *
+ *@dest: the first string
+ *@src: the second one
+ * Return: Always ptr
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr;
+
+	ptr = strcat(dst, src);
+
+	return (ptr);
+}
