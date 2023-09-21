b52af6ba47cfed723229a19a5e0568f53beb232a
abdelghafour_99 <abdelghafour83@live.fr>
2023-09-21 15:03:45 -0700

    0x06. C - More pointers, arrays and strings commit 555

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- * string_toupper - change lowercase
+ * cap_string - change lowercase
  * letters of a string to uppercase.
  *
  *@a: pointer
  * Return: alwas a
  */
 
-char *string_toupper(char *a)
+char *cap_string(char *a)
 {
 	int i = 0;
 
 	while (a[i] != '\0')
 	{
-		if (a[i] <= 'z' && a[i] >= 'a')
-			a[i] = a[i] - 32;
+		if (a[i] <= 'Z' && a[i] >= 'A')
+			a[i] = a[i] + 32;
 	}
 	return (a);
 
