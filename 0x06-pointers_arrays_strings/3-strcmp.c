0d68ea7fa4c9119bce4193b849689395f913f739
abdelghafour_99 <abdelghafour83@live.fr>
2023-09-21 17:30:28 -0700

    0x06. C - More pointers, arrays and strings commit 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,13 @@
  * Return: Always s1[a] - s2[a]
  */
 
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	int a = 0;
 
-	while (s1[a] != '\0' && s2[a] != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (s1[a] != s2[a])
-		{
-			return (s1[a]);
-		}
-		a++;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 	}
 
 	return (0);
