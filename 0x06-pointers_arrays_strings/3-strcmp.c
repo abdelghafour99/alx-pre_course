1b9a91e2a1585fa7241c5afc41575ef0d8b34137
abdelghafour_99 <abdelghafour83@live.fr>
2023-09-21 17:39:30 -0700

    0x06. C - More pointers, arrays and strings commit 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int a = 0;
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (s1[a] != '\0' && s2[a] != '\0')
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
+		if (s1[a] != s2[a])
+			return (s1[a] - s2[a]);
+		a++;
 	}
 
 	return (0);
